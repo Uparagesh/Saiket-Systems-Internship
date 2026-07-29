b54585ee21e68f51936e97e0b98492e8c573fa44
Uparagesh <upadhyayragesh@gmail.com>
2026-07-29 13:34:49 +0530

    Completed Task 6 - Nike Landing Page Clone

---
+++ Task-6-Webpage-Clone/script.js
@@ -0,0 +1,5 @@
+const buyBtn = document.getElementById("buyBtn");
+
+buyBtn.addEventListener("click", function(){
+    alert("Thank you for choosing Nike! Your order has been added to the cart.")
+});
